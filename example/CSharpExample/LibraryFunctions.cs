45181535fa41c5d3f1b9b3bf182f823c21b023e0
Steve Cheng <steve.ckp@gmail.com>
2020-07-18 20:54:40 -0400

    Add simple .NET library to test

---
+++ example/CSharpExample/LibraryFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace CSharpExample
+{
+    public static class LibraryFunctions
+    {
+        public static int PrintHelloWorld(IntPtr argPtr, int argSize)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"Hello world! 世界に挨拶します　argPtr={argPtr} argSize={argSize}");
+            return 0;
+        }
+    }
+}
