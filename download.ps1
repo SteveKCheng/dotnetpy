e911ac60ca23c6c15bf34512900a390c64be20d5
Steve Cheng <steve.ckp@gmail.com>
2020-07-20 10:36:53 -0400

    Force TLS 1.2 when downloading in PowerShell (through proxy)

--- download.ps1
+++ download.ps1
@@ -1,8 +1,18 @@
 # PowerShell script for HTTP download
 # for Windows systems with corporate HTTP proxies
 param ([Parameter(Mandatory=$true)][string]$url, [Parameter(Mandatory=$true)][string]$file)
+
 $ErrorActionPreference = "Stop"
+
 $client = New-Object System.Net.WebClient
+
 $client.Proxy = [System.Net.WebRequest]::GetSystemWebProxy()
 $client.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+
+# Force TLS 1.2 for badly implemented proxies
+if ($client.Proxy.GetProxy($url))
+{
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+}
+
 $client.DownloadFile($url, $file)
